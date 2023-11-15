c97106d134132adbce093f35a4e6f65307aa8179
Michael Nowak <michael.nowak@solidigmtechnology.com>
2023-11-15 12:15:21 -0800

    feat: implement example logging

---
+++ main.py
@@ -0,0 +1,62 @@
+import datetime
+import logging
+import structlog
+
+def example_logging_basic():
+    # default simple setup
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger('example')
+
+    logger.info("""example_logging_basic:
+
+# default simple setup
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('example')
+""")
+
+    # test logging
+    logger.log(logging.NOTSET, 'this is a notset log')
+    logger.debug('this is a debug log')
+    logger.info('this is an info log')
+    logger.warning('this is a warning log')
+    logger.error('this is an error log')
+    logger.critical('this is a critical log')
+
+    logger.info('end of example_logging_basic')
+
+
+def example_logging_structured():
+    # structlog setup
+    def timestamper(_, __, event_dict):
+        event_dict["time"] = datetime.datetime.now().isoformat()
+        return event_dict
+
+    structlog.configure(processors=[
+        timestamper,
+        structlog.processors.add_log_level,
+        structlog.processors.JSONRenderer()
+        ])
+    logger = structlog.get_logger()
+
+    logger.info("""example_logging_structured:
+
+# structlog setup
+structlog.configure(processors=[structlog.processors.JSONRenderer()])
+logger = structlog.get_logger()
+""")
+
+    # test logging
+    logger.debug('this is a debug log')
+    logger.info('this is an info log')
+    logger.warning('this is a warning log')
+    logger.error('this is an error log')
+    logger.critical('this is a critical log')
+
+    logger.info('end of example_logging_structured')
+
+
+if __name__ == '__main__':
+    example_logging_basic()
+
+    # show the example for structured logging
+    example_logging_structured()
